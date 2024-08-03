0019cca1d5a82d8ec8db5c46d2fb40b1599fdbe2
TushikaKhanna <tushika.khanna@dcmail.ca>
2024-08-02 21:57:46 -0400

    final update

--- aasignment4-p2/gallery-start/main.js
+++ aasignment4-p2/gallery-start/main.js
@@ -13,10 +13,13 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+
 const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 /* Declaring the alternative text for each image file */
+
 const alt = ["pic 1", "pic 2", "pic 3", "pic 4", "pic 5"];
 /* Looping through images */
+
 for (const image of images) {
 const newImage = document.createElement('img');
 newImage.setAttribute('src', `images/${image}`);
